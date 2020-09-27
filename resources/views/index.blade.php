deff7d2a2194e7d2e84fd94c000997b086fc86b4
Tailon Perin <tailonedson@gmail.com>
2020-09-26 23:36:58 -0300

    css and index test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
-    This is the index
-
+    <div class="flex flex-col">
+        <div class="text-center text-lg text-gray-900">Bem vindo ao Asteroid v1.0</div>
+    </div>
 @endsection
