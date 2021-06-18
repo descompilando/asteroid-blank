83c9de503b343e35743f393ca7a752cd45cd9927
Daniel Freitas <daniel1208x@gmail.com>
2021-06-17 23:16:49 -0300

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const tailwindcss = require('tailwindcss');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-mix.sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
     .options({
         postCss: [tailwindcss('tailwind.config.js')],
-    });
+    })
